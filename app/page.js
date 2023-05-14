64f701ef0efca58d316f4a30cbb776eb33cf04dc
chatmoon <chatmoon3@gmail.com>
2023-05-14 22:44:34 +0900

    mongoDB 연동 완료

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
+import { connectDB } from "@/util/database.js"
 
-export default function Home() {
+export default async function Home() {
+
+  const db = (await connectDB).db('forum')
+  let result = await db.collection('post').find().toArray()
+  console.log(result)
+
   return (
     <div>홈페이지</div>
   )
